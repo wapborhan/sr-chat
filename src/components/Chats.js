c906dea28fd2d964f5aa8de42cae5cc24bbfc121
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-08-20 00:03:44 +0600

    Update

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,21 +1,86 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { ChatEngine } from "react-chat-engine";
 import { auth } from "./firebase";
 
 import { useAuth } from "../context/AuthContex";
+import axios from "axios";
 
 const Chats = () => {
   const history = useHistory();
   const { user } = useAuth();
+  const [loading, setLoading] = useState(true);
 
-  console.log(user);
+  // console.log(user);
 
   const handleLogout = async () => {
     await auth.signOut();
 
     history.push("/");
   };
+
+  const getFile = async (url) => {
+    const response = await fetch(url);
+    const data = await response.blob();
+
+    return new File([data], "userPhoto.jpg", { type: "image/jpeg" });
+  };
+
+  useEffect(() => {
+    if (!user) {
+      history.push("/");
+
+      return;
+    }
+
+    axios
+      .get("https://api.chatengine.io/users/me", {
+        headers: {
+          "project-id": "cec488d0-4191-435c-9358-181040165498",
+          "user-name": user.email,
+          "user-secret": user.uid,
+        },
+      })
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(() => {
+        let formdata = new FormData();
+        formdata.append("email", user.email);
+        formdata.append("username", user.displayName);
+        formdata.append("secret", user.uid);
+
+        getFile(user.photoURL).then((avatar) => {
+          formdata.append("avatar", avatar, avatar.name);
+
+          axios
+            .post("https://api.chatengine.io/users/", formdata, {
+              headers: {
+                "private-key": "93e88a72-2d1a-4389-8cb5-90ff65b4eeb8",
+              },
+            })
+            .then(() => {
+              setLoading(false);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
+      });
+  }, [user, history]);
+
+  if (!user || loading)
+    return (
+      <div className="chats-page">
+        <div className="nav-bar">
+          <div className="logo-tab">SR Chat</div>
+        </div>
+        <div className="logout-tab" onClick={handleLogout}>
+          Logout
+        </div>
+        <div className="load">Loading.....</div>
+      </div>
+    );
 
   return (
     <div className="chats-page">
@@ -27,10 +92,10 @@ const Chats = () => {
       </div>
       <ChatEngine
         height="calc(100vh-66px"
-        projectId="
+        projectID="
 cec488d0-4191-435c-9358-181040165498"
-        userName="."
-        userSecret="."
+        userName={user.email}
+        userSecret={user.uid}
       />
     </div>
   );
